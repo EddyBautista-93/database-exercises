26c35ce09baa7a0316f9cadb62eb08aa56c65c81
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-10-09 15:38:42 -0500

    completed exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+CREATE TABLE If NOT EXISTS quotes (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(25),
+    name VARCHAR(20),
+    release_date DATE ,
+    sales float ,
+    genre varchar(20),
+
+    PRIMARY KEY (id)
+);
