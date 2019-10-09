f578f5105cad940ebe8546a8ad47f516be07f167
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-10-09 15:53:59 -0500

    added name change

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
-CREATE TABLE If NOT EXISTS quotes (
+CREATE TABLE If NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(25),
     name VARCHAR(20),
