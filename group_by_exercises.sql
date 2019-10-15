347616d21c635d8cf2227747783198d4d7c52907
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-10-15 11:18:50 -0500

    completed group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,3 +9,18 @@ FROM employees
 WHERE last_name like 'E%e'
 GROUP BY last_name, first_name;
 
+SELECT DISTINCT last_name, count(*)
+FROM employees
+WHERE last_name like '%q%' and last_name not like '%qu%'
+group by last_name
+ORDER BY COUNT(*);
+
+SELECT DISTINCT  gender,count(*)
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya'
+group by gender
+ORDER BY COUNT(*) ;
+
+
+
+
