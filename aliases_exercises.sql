ba22731bb441ec279fe2d1530045555284052c32
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-10-15 11:29:22 -0500

    addd changes;

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,7 +1,8 @@
 USE employees;
 
-SELECT CONCAT(emp_no,' - ',first_name, ' ', last_name) AS full_name, birth_date as DOB
-FROM employees AS emp
-GROUP BY full_name, last_name, birth_date
-ORDER BY last_name
+SELECT CONCAT(emp_no,' - ',last_name, ' ', first_name) AS full_name, birth_date as DOB
+FROM employees
+# WHERE emp_no between 10001 and 10010
+# GROUP BY emp_no, last_name, birth_date
+# ORDER BY last_name
 LIMIT 10;
